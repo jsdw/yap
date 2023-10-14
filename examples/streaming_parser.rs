1fe07855620f6701537f7db25ceb5fa64efa6209
Easyoakland <97992568+Easyoakland@users.noreply.github.com>
2023-10-14 04:38:14 -0600

    feat: StreamTokens for non-cloneable iterators
    - Use `StreamTokens` to parse on non-cloneable iterators
    such as a network socket or user input
    - An example fizzbuzz program that parses from stdout is included

--- examples/streaming_parser.rs
+++ examples/streaming_parser.rs
@@ -1,167 +1,8 @@
 use std::{
-    collections::VecDeque,
-    fmt::Debug,
+    fmt::Display,
     io::{stdin, Read},
-    iter::Iterator,
-    sync::{Arc, Mutex},
 };
-use yap::{TokenLocation, Tokens};
-
-/// Buffer over items of an iterator.
-#[derive(Clone, Debug, PartialEq, Eq)]
-struct Buffer<Item> {
-    oldest_elem_id: usize,
-    elements: VecDeque<Option<Item>>,
-    /// Sorted list of the oldest items needed per location
-    checkout: Vec<usize>,
-}
-
-impl<Item> Default for Buffer<Item> {
-    fn default() -> Self {
-        Self {
-            oldest_elem_id: Default::default(),
-            elements: Default::default(),
-            checkout: Default::default(),
-        }
-    }
-}
-
-/// Buffers over an iterator that can't itself be cloned.
-/// Enables parsing a stream of values such such as from a network socket or other realtime source.
-#[derive(Clone, Debug)]
-pub struct BufferedTokens<I>
-where
-    I: Iterator,
-{
-    iter: I,
-    cursor: usize,
-    /// Shared buffer of items and the id of the oldest item in the buffer.
-    buffer: Arc<Mutex<Buffer<I::Item>>>,
-}
-
-/// Token location that stores the changes since it was created for when the iterator is reset.
-#[derive(Clone, Debug)]
-pub struct BufferedTokensLocation<Item> {
-    cursor: usize,
-    buffer: Arc<Mutex<Buffer<Item>>>,
-}
-
-impl<Item> PartialEq for BufferedTokensLocation<Item> {
-    fn eq(&self, other: &Self) -> bool {
-        self.cursor == other.cursor && Arc::ptr_eq(&self.buffer, &other.buffer)
-    }
-}
-impl<Item> Eq for BufferedTokensLocation<Item> {}
-
-impl<Item> Drop for BufferedTokensLocation<Item> {
-    fn drop(&mut self) {
-        // Would rather not free memory then panic this thread because another panicked.
-        if let Ok(mut buf) = self.buffer.lock() {
-            let idx = buf
-                .checkout
-                .binary_search(&self.cursor)
-                .expect("missing entry for location in checkout");
-            buf.checkout.remove(idx);
-        }
-    }
-}
-
-impl<Item> TokenLocation for BufferedTokensLocation<Item> {
-    fn offset(&self) -> usize {
-        self.cursor
-    }
-}
-
-impl<I: Iterator> BufferedTokens<I> {
-    pub fn into_tokens(iter: I) -> Self {
-        BufferedTokens {
-            iter,
-            cursor: Default::default(),
-            buffer: Default::default(),
-        }
-    }
-}
-
-impl<I> Tokens for BufferedTokens<I>
-where
-    I: Iterator,
-    I::Item: Clone + Debug,
-{
-    type Item = I::Item;
-
-    type Location = BufferedTokensLocation<I::Item>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.cursor += 1;
-
-        // Try buffer
-        {
-            let buf = self.buffer.lock().expect("not poisoned");
-            // If buffer has elements use buffer before getting new elements.
-            if let Some(val) = buf.elements.get(self.cursor - buf.oldest_elem_id).cloned() {
-                return val;
-            }
-        }
-
-        // Clear buffer of old values
-        {
-            let mut buf = self.buffer.lock().expect("not poisoned");
-            // Remove old values no longer needed by any location
-            let min = match buf.checkout.first() {
-                Some(&x) => x.min(self.cursor),
-                None => self.cursor,
-            };
-            while (buf.oldest_elem_id < min) && (!buf.elements.is_empty()) {
-                buf.elements.pop_front();
-                buf.oldest_elem_id += 1;
-            }
-        }
-
-        // Handle cache miss
-        {
-            let next = self.iter.next();
-            let mut buf = self.buffer.lock().expect("not poisoned");
-            // Don't save to buffer if no locations exist which might need the value again
-            if buf.checkout.is_empty() {
-                return next;
-            } else {
-                buf.elements.push_back(next.clone());
-                next
-            }
-        }
-    }
-
-    fn location(&self) -> Self::Location {
-        // Checkout value at current location
-        let mut buf = self.buffer.lock().expect("not poisoned");
-        match buf.checkout.binary_search(&self.cursor) {
-            Ok(x) => buf.checkout.insert(x, self.cursor),
-            Err(x) => buf.checkout.insert(x, self.cursor),
-        };
-        BufferedTokensLocation {
-            cursor: self.cursor,
-            buffer: Arc::clone(&self.buffer),
-        }
-    }
-
-    fn set_location(&mut self, location: Self::Location) {
-        // Update cursor to new value
-        self.cursor = location.cursor;
-        // Location removes itself from checkout on drop
-    }
-
-    fn is_at_location(&self, location: &Self::Location) -> bool {
-        self.cursor == location.cursor
-    }
-}
-
-/// Takes two digits and adds them together. Fails if there are not two base 10 digits.
-fn parse_digit_pair(tokens: &mut impl Tokens<Item = u8>) -> Option<u32> {
-    let d1 = tokens.next()? as char;
-    let d2 = tokens.next()? as char;
-    // Return the result of adding the 2 digits we saw:
-    Some(d1.to_digit(10)? + d2.to_digit(10)?)
-}
+use yap::{types::StreamTokens, Tokens};
 
 /// Parses a line ending of either "\r\n" (windows) or "\n" (linux)
 fn line_ending(tokens: &mut impl Tokens<Item = u8>) -> Option<&'static str> {
@@ -171,33 +12,87 @@ fn line_ending(tokens: &mut impl Tokens<Item = u8>) -> Option<&'static str> {
     )
 }
 
-fn main() {
-    let stdin = stdin().bytes().map(Result::unwrap);
-    let mut tokens = BufferedTokens::into_tokens(stdin);
-    // Set a location here to show how buffering works.
-    let start = tokens.location();
-    // Demonstrate streaming parsing of input.
-    // This is relatively painless and looks the same as a non-streaming parser because `BufferedTokens` handles buffering and `Bytes<Stdin>` handles blocking.
-    // Parse pairs of digits into their sum. Stop parsing when a non-pair of digits is encountered.
-    for line_of_digits in tokens.sep_by(
-        |t| Some(t.many(|t| parse_digit_pair(t)).collect::<Vec<_>>()),
-        |t| line_ending(t).is_some(),
-    ) {
-        println!("Line of digits: {:?}", line_of_digits);
-    }
-    // Reset location to show usage of internal buffer and freeing memory once the location doesn't need a diff buffer.
-    tokens.set_location(start);
-    // Internal buffer is cleared since there are no longer locations which need the items.
-    while let Some(x) = tokens.as_iter().next() {
-        println!("Element: {x}");
-        if tokens
-            .buffer
-            .lock()
-            .expect("not poisoned")
-            .elements
-            .is_empty()
-        {
-            break;
+#[derive(Debug)]
+enum FizzBuzz {
+    Fizz,
+    Buzz,
+    FizzBuzz,
+    Neither,
+}
+
+impl Display for FizzBuzz {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            FizzBuzz::Fizz => write!(f, "fizz"),
+            FizzBuzz::Buzz => write!(f, "buzz"),
+            FizzBuzz::FizzBuzz => write!(f, "fizzbuzz"),
+            FizzBuzz::Neither => write!(f, "neither"),
         }
     }
+}
+
+/// Takes two digits and adds them together. Fails if there are not two base 10 digits.
+fn fizzbuzz(x: u32) -> FizzBuzz {
+    if x % 5 == 0 && x % 3 == 0 {
+        FizzBuzz::FizzBuzz
+    } else if x % 3 == 0 {
+        FizzBuzz::Fizz
+    } else if x % 5 == 0 {
+        FizzBuzz::Buzz
+    } else {
+        FizzBuzz::Neither
+    }
+}
+
+/// An example program that parses stdin for numbers that match an output by the rules of fizzbuzz.
+/// It prints as it parses so the incremental parsing is more obvious.
+fn main() {
+    let stdin = stdin().bytes().map(Result::unwrap);
+    // Can't use `stdin.clone()` because it is over a stream of incoming values.
+    // If we could clone then [`IterTokens`] would be prefferable.
+    let mut tokens = StreamTokens::into_tokens(stdin);
+    let mut parsed_result = Vec::new();
+
+    println!("Lets play fizzbuzz! Enter a number. If it is divisible by three I'll say \"fizz\", \
+if it is divisible by five I'll say \"buzz\", and if it is divisible by both 3 and 5 I'll say \"fizzbuzz\".");
+
+    // Set a location to allow rewinding back to this point later.
+    // All items since the oldest location (`start` in this case) will be buffered
+    // so that a rewind to this point can occur with `set_location` if needed.
+    let start = tokens.location();
+
+    // Demonstrate streaming parsing of input.
+    // This is relatively painless and looks the same as a non-streaming parser.
+    // `StreamTokens` handles buffering and `Bytes<Stdin>` handles blocking in this case.
+    // The general form is some iterator over new input and wrapping that in `StreamTokens`
+    // to make that stream of items parseable.
+    for num in tokens.sep_by(
+        |t| {
+            Some(
+                t.tokens_while(|x| x.is_ascii_digit())
+                    .map(|x| x as char)
+                    .collect::<String>()
+                    .parse::<u32>(),
+            )
+        },
+        |t| line_ending(t).is_some(),
+    ) {
+        match num {
+            Ok(x) => {
+                let res = fizzbuzz(x);
+                println!("{}", res);
+                parsed_result.push(res)
+            }
+            Err(_) => println!("Ending parse on invalid u64"),
+        }
+    }
+
+    // Can use location to reset to previous values because they have been internally buffered
+    let previous_tokens = tokens
+        .slice(start, tokens.location())
+        .as_iter()
+        .map(|x| x as char)
+        .collect::<String>();
+    println!("You entered:\n{previous_tokens}");
+    println!("This parsed as: {parsed_result:?}");
 }
--- src/types.rs
+++ src/types.rs
@@ -268,6 +268,229 @@ where
     }
 }
 
+#[cfg(feature = "std")]
+pub use stream_tokens::{StreamTokens, StreamTokensLocation};
+#[cfg(feature = "std")]
+mod stream_tokens {
+    use crate::{IntoTokens, TokenLocation, Tokens};
+    use std::{
+        collections::VecDeque,
+        fmt::Debug,
+        iter::Iterator,
+        sync::{Arc, Mutex},
+    };
+
+    /// Buffer over items of an iterator.
+    #[derive(Clone, Debug, PartialEq, Eq)]
+    struct Buffer<Item> {
+        oldest_elem_id: usize,
+        elements: VecDeque<Option<Item>>,
+        /// Sorted list of the oldest items needed per location
+        checkout: Vec<usize>,
+    }
+
+    // Manual impl because Item: !Default also works.
+    impl<Item> Default for Buffer<Item> {
+        fn default() -> Self {
+            Self {
+                oldest_elem_id: Default::default(),
+                elements: Default::default(),
+                checkout: Default::default(),
+            }
+        }
+    }
+
+    /// Enables parsing a stream of values from an iterator that can't itself be cloned.
+    /// In order to be able to rewind the iterator it must save values since the last created [`StreamTokensLocation`]
+    #[derive(Clone, Debug)]
+    pub struct StreamTokens<I>
+    where
+        I: Iterator,
+    {
+        iter: I,
+        cursor: usize,
+        /// Shared buffer of items and the id of the oldest item in the buffer.
+        buffer: Arc<Mutex<Buffer<I::Item>>>,
+    }
+
+    /// This implements [`TokenLocation`] and stores the location. It also marks the [`Iterator::Item`]s
+    /// since it was created to be stored for when the corresponding [`StreamTokens`] is reset.
+    ///
+    /// The location is equivalent to `offset` in [`Iterator::nth(offset)`].
+    ///
+    /// The [`Drop`] implementation will un-mark that [`Iterator::Item`]s must be stored,
+    /// allowing the originating [`StreamTokens`] to drop old values and free memory.
+    #[derive(Debug)]
+    pub struct StreamTokensLocation<Item> {
+        cursor: usize,
+        buffer: Arc<Mutex<Buffer<Item>>>,
+    }
+
+    impl<Item> Clone for StreamTokensLocation<Item> {
+        fn clone(&self) -> Self {
+            // Checkout the cursor's position again
+            let mut buf = self.buffer.lock().expect("not poisoned");
+            let idx = match buf.checkout.binary_search(&self.cursor) {
+                Ok(x) | Err(x) => x,
+            };
+            buf.checkout.insert(idx, self.cursor);
+            // Then copy
+            Self {
+                cursor: self.cursor.clone(),
+                buffer: self.buffer.clone(),
+            }
+        }
+    }
+
+    impl<Item> PartialEq for StreamTokensLocation<Item> {
+        fn eq(&self, other: &Self) -> bool {
+            self.cursor == other.cursor
+        }
+    }
+    impl<Item> Eq for StreamTokensLocation<Item> {}
+
+    impl<Item> Drop for StreamTokensLocation<Item> {
+        fn drop(&mut self) {
+            // Would rather not free memory then panic this thread because another panicked.
+            if let Ok(mut buf) = self.buffer.lock() {
+                // Remove self.cursor from checkout.
+                let idx = buf
+                    .checkout
+                    .binary_search(&self.cursor)
+                    .expect("missing entry for location in checkout");
+                buf.checkout.remove(idx);
+            }
+        }
+    }
+
+    impl<Item> TokenLocation for StreamTokensLocation<Item> {
+        fn offset(&self) -> usize {
+            self.cursor
+        }
+    }
+
+    impl<I: Iterator> StreamTokens<I>
+    where
+        I::Item: Clone,
+    {
+        /// We can't define a blanket impl for [`IntoTokens`] on all `impl Iterator<Item: Clone>` without
+        /// [specialization](https://rust-lang.github.io/rfcs/1210-impl-specialization.html).
+        ///
+        /// Instead, use this method to convert a suitable iterator into [`Tokens`].
+        ///
+        /// # Example
+        ///
+        /// ```rust
+        /// use yap::{ Tokens, types::StreamTokens };
+        ///
+        /// // In normal usage, "hello \n\t world".into_tokens()
+        /// // would be preferred here (which would give StrTokens).
+        /// // This is just to demonstrate using StreamTokens:
+        /// let chars_iter = "hello \n\t world".chars();
+        /// let mut tokens = StreamTokens::into_tokens(chars_iter);
+        ///
+        /// // now we have tokens, we can do some parsing:
+        /// assert!(tokens.tokens("hello".chars()));
+        /// tokens.skip_tokens_while(|c| c.is_whitespace());
+        /// assert!(tokens.tokens("world".chars()));
+        /// ```
+        pub fn into_tokens(iter: I) -> Self {
+            StreamTokens {
+                iter,
+                cursor: Default::default(),
+                buffer: Default::default(),
+            }
+        }
+    }
+
+    impl<I> Tokens for StreamTokens<I>
+    where
+        I: Iterator,
+        I::Item: Clone + Debug,
+    {
+        type Item = I::Item;
+
+        type Location = StreamTokensLocation<I::Item>;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            self.cursor += 1;
+
+            // Try buffer
+            {
+                let buf = self.buffer.lock().expect("not poisoned");
+                // If buffer has needed element use buffer before getting new elements.
+                if let Some(val) = buf
+                    .elements
+                    .get(self.cursor - 1 - buf.oldest_elem_id)
+                    .cloned()
+                {
+                    return val;
+                }
+            }
+
+            // Clear buffer of old values
+            {
+                let mut buf = self.buffer.lock().expect("not poisoned");
+                // Remove old values no longer needed by any location
+                let min = match buf.checkout.first() {
+                    Some(&x) => x.min(self.cursor),
+                    None => self.cursor,
+                };
+                while (buf.oldest_elem_id < min) && (!buf.elements.is_empty()) {
+                    buf.elements.pop_front();
+                    buf.oldest_elem_id += 1;
+                }
+            }
+
+            // Handle cache miss
+            {
+                let next = self.iter.next();
+                let mut buf = self.buffer.lock().expect("not poisoned");
+                // Don't save to buffer if no locations exist which might need the value again
+                if buf.checkout.is_empty() {
+                    next
+                } else {
+                    buf.elements.push_back(next.clone());
+                    next
+                }
+            }
+        }
+
+        fn location(&self) -> Self::Location {
+            // Checkout value at current location
+            let mut buf = self.buffer.lock().expect("not poisoned");
+            match buf.checkout.binary_search(&self.cursor) {
+                Ok(x) | Err(x) => buf.checkout.insert(x, self.cursor),
+            };
+            StreamTokensLocation {
+                cursor: self.cursor,
+                buffer: Arc::clone(&self.buffer),
+            }
+        }
+
+        fn set_location(&mut self, location: Self::Location) {
+            // Update cursor to new value
+            self.cursor = location.cursor;
+            // Location removes itself from checkout on drop
+        }
+
+        fn is_at_location(&self, location: &Self::Location) -> bool {
+            self.cursor == location.cursor
+        }
+    }
+
+    impl<I> IntoTokens<I::Item> for StreamTokens<I>
+    where
+        I: Iterator,
+        I::Item: Clone + core::fmt::Debug,
+    {
+        type Tokens = Self;
+        fn into_tokens(self) -> Self {
+            self
+        }
+    }
+}
+
 /// Embed some context with your [`Tokens`] implementation to
 /// access at any time. Use [`Tokens::with_context`] to produce this.
 pub struct WithContext<T, C> {
